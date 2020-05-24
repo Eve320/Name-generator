2322af0640b88a38e8304d260e25fa2f888ad217
Eve320 <enduku98@gmail.com>
2020-05-24 15:06:50 -0700

    Add initial variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 var maleArray=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleArray=["AKosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+var year=parseInt(document.getElementById("year").value);
+var month=parseInt(document.getElementById("month").value);
+var day-parseInt(document.getElementById("day").value);
+var dayOfTheWeek;
+
+
+var CC = year.slice(0,2);
+var YY = year.slice(1, 3);
