b4bfa58dd89c303c19c33258388af21a3190232b
Eve320 <enduku98@gmail.com>
2020-05-25 01:37:37 -0700

    Changing form class

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 var maleArray=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleArray=["AKosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-var year=parseInt(document.getElementById("year").value);
-var month=parseInt(document.getElementById("month").value);
-var day-parseInt(document.getElementById("day").value);
-var dayOfTheWeek;
+function getName(){
+ var year=document.getElementById("year").value;
+ var month=document.getElementById("month").value;
+ var date=document.getElementById("date").value;
+}
+
 
 
 var CC = year.slice(0,2);
-var YY = year.slice(1, 3);
+var YY = year.slice(1,3);
+
+function calculate(){
+  var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  return(Math.floor(dayOfTheWeek))
+  alert(dayOfTheWeek)
+}
