8e62799f91872a8247b13663ac85e1fd29a31ec5
Eve320 <enduku98@gmail.com>
2020-05-25 01:57:55 -0700

    Add gender function

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,28 @@ var maleArray=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleArray=["AKosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
 function getName(){
- var year=document.getElementById("year").value;
- var month=document.getElementById("month").value;
- var date=document.getElementById("date").value;
+ var year = parseInt(document.getElementById("year").value);
+ var month = parseInt(document.getElementById("month").value);
+ var date = parseInt(document.getElementById("date").value);
+
 }
 
 
+var cc = parseInt(document.getElementById("year").value.slice(0,2));
+var yy = parseInt(document.getElementById("year").value.slice(2,4));
 
-var CC = year.slice(0,2);
-var YY = year.slice(1,3);
+var dayOfTheWeek= math.floor()( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7);
 
-function calculate(){
-  var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7;
-  return(Math.floor(dayOfTheWeek))
-  alert(dayOfTheWeek)
-}
+
+var checkgender = function() {
+
+    var gndr = document.getElementsByName("one");
+    if(gndr[0].checked == true){
+        gender = "male";
+    }else if(gndr[1].checked == true ){
+        gender = "female";
+    }
+    else {
+        alert("Input gender")
+      }
+    }
